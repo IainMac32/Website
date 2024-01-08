d643c74f00e09d4930c5cae15c618f0252787d82
Iain Macdonald <65032680+IainMac32@users.noreply.github.com>
2024-01-08 00:10:18 -0500

    c

--- src/App.js
+++ src/App.js
@@ -354,7 +354,7 @@ const StyledHeader = styled.h1`
           padding: 0,
           justifyContent: "space-evenly",
           position: "fixed",
-          background: "#2F4B63",
+          background: "rgb(70,93,110)",
           borderRight: "3px solid black",
         }}>
           <StyledHeader>Iain Macdonald</StyledHeader>
